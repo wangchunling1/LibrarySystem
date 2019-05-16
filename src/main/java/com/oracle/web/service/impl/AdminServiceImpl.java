812412ea54e09c6330db26b769a3a018b096d24b
LiLeYao <LiLeYao@qq.com>
2019-05-17 07:55:37 +0800

    李乐瑶第一次提交ssm

--- src/main/java/com/oracle/web/service/impl/AdminServiceImpl.java
+++ src/main/java/com/oracle/web/service/impl/AdminServiceImpl.java
@@ -1,7 +1,37 @@
 package com.oracle.web.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.oracle.mapper.AdminMapper;
+import com.oracle.web.bean.Admin;
 import com.oracle.web.service.AdminService;
 
 public class AdminServiceImpl implements AdminService {
+	
+	@Autowired
+	private AdminMapper adminMapper;
+
+	@Override
+	@Transactional
+	public int save(Admin admin) {
+		// TODO Auto-generated method stub
+		return this.adminMapper.save(admin);
+	}
+
+	@Override
+	@Transactional(readOnly=true)
+	public Admin queryone(String userName) {
+		// TODO Auto-generated method stub
+		return this.adminMapper.queryOne(userName);
+	}
+
+	@Override
+	@Transactional
+	public Admin login(Admin admin) {
+		// TODO Auto-generated method stub
+		return this.adminMapper.login(admin);
+		
+	}
 
 }
