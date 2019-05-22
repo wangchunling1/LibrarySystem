448a34e2bf09616229596a746e7cd13e65ff9a77
LiLeYao <LiLeYao@qq.com>
2019-05-22 18:36:57 +0800

    完成了管理员的登录注册

--- src/main/java/com/oracle/web/service/AdminService.java
+++ src/main/java/com/oracle/web/service/AdminService.java
@@ -4,4 +4,10 @@ import com.oracle.web.bean.Admin;
 
 public interface AdminService {
 
+	int save(Admin admin);
+
+	Admin queryOne(String username);
+
+	Admin login(Admin admin);
+
 }
--- src/main/java/com/oracle/web/service/impl/AdminServiceImpl.java
+++ src/main/java/com/oracle/web/service/impl/AdminServiceImpl.java
@@ -1,7 +1,39 @@
 package com.oracle.web.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.oracle.mapper.AdminMapper;
+import com.oracle.web.bean.Admin;
 import com.oracle.web.service.AdminService;
 
+@Service
 public class AdminServiceImpl implements AdminService {
+	
+	@Autowired
+	private AdminMapper adminMapper;
+
+	@Override
+	@Transactional
+	public int save(Admin admin) {
+		// TODO Auto-generated method stub
+		return this.adminMapper.save(admin);
+	}
+
+	@Override
+	@Transactional(readOnly=true)
+	public Admin queryOne(String username) {
+		// TODO Auto-generated method stub
+		return this.adminMapper.queryOne(username);
+	}
+
+	@Override
+	@Transactional
+	public Admin login(Admin admin) {
+		// TODO Auto-generated method stub
+		return this.adminMapper.login(admin);
+		
+	}
 
 }
